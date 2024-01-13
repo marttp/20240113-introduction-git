799ad7c0d9f7d378d527e330a96378433a80a62f
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-01-13 18:11:32 +0900

    Rearrange codebase

--- minus.js
+++ minus.js
@@ -1,10 +1,6 @@
-let a = 1;
-let b = 2;
-
-function calculateMinus(num1,num2) {
+function calculateMinus(num1, num2) {
     return num1 - num2;
 }
 
-console.log(calculateMinus(a,b));
 
-export { calculateMinus };
+module.exports = { calculateMinus };
