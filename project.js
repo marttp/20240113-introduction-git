f24fb2488131a6c89440941effa027420d1d5c20
pitchayapornpei <pitchayapornpei35@gmail.com>
2024-01-13 16:22:27 +0700

    version-2: update

--- project.js
+++ project.js
@@ -1,4 +1,5 @@
 const { add } = require('./calculator.js');
+const { multiply } = require('./calculator.js');
 
 console.log("Please kindly help add below function");
 
@@ -8,5 +9,6 @@ let a = 5;
 let b = 10;
 
 console.log(add(a, b));
+console.log(multiply(a, b));
 
 // TODO: Simple area calculation - Rectangle, Triangle, Circle
